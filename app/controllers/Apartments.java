0be1e07b43ed9d2f97804b0658ee2c56d2e41fe0
LArchaon <alex.buzdin@gmail.com>
2013-05-22 22:00:42 +0300

    Added new Apartment feature

--- app/controllers/Apartments.java
+++ app/controllers/Apartments.java
@@ -12,8 +12,8 @@ public class Apartments extends Controller {
 
     static Form<Apartment> apartmentForm = Form.form(Apartment.class);
 
-    public static Result newApartment() {
-        return ok(views.html.apartment_new.render(apartmentForm));
+    public static Result newApartment(Long hotelId) {
+        return ok(views.html.apartment_new.render(hotelId, apartmentForm));
     }
 
     public static Result openApartment(Long id) {
@@ -38,12 +38,21 @@ public class Apartments extends Controller {
         return redirect(routes.Hotels.allHotel());
     }
 
-    public static Result saveApartment() {
+    public static Result saveApartment(Long hotelId) {
         Form<Apartment> filledForm = apartmentForm.bindFromRequest();
         if(filledForm.hasErrors()) return badRequest(filledForm);
 
-        Apartment.save(filledForm.get());
-        return redirect(routes.Hotels.allHotel());
+        Apartment apartment = filledForm.get();
+        Hotel hotel = Hotel.get(hotelId);
+        apartment.setHotel(hotel);
+
+        String apartmentTypeId = Form.form().bindFromRequest().get("apartmentTypeId");
+        ApartmentType apartmentType = ApartmentType.get(Long.valueOf(apartmentTypeId));
+
+        apartment.setApartmentType(apartmentType);
+
+        Apartment.save(apartment);
+        return redirect(routes.Hotels.openHotel(hotelId));
     }
 
     public static Result deleteApartment(Long id) {
