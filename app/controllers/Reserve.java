64e70036af0ceb8de22c226ed6a54140f2aab521
LArchaon <alex.buzdin@gmail.com>
2013-05-31 20:12:45 +0300

    Added Reserve Hotel selection

--- app/controllers/Reserve.java
+++ app/controllers/Reserve.java
@@ -1,13 +1,24 @@
 package controllers;
 
+import models.Hotel;
 import play.mvc.Result;
+
+import java.util.ArrayList;
 
 import static play.mvc.Results.ok;
 
 public class Reserve {
 
-    public static Result inquiryForm() {
-        return ok(views.html.reserve.render());
+    public static Result reserve() {
+        return ok(views.html.reserve.render(Hotel.all()));
+    }
+
+    public static Result reserveHotel(Long id) {
+        return ok(views.html.reserve_hotel.render(Hotel.get(id), new ArrayList()));
+    }
+
+    public static Result reserveApartmentType() {
+        return ok(views.html.reserve.render(Hotel.all()));
     }
 
 }
